920e3cd9a29da9d14871b2ef123e16a936d10190
Karan Rajpal <rajpal.karan73@gmail.com>
2019-12-28 16:02:50 -0600

    Added MDX compatibility

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,27 @@
  */
 
 module.exports = {
-  /* Your site config here */
-}
+
+  siteMetadata: {
+    title: `The Localhost Blog`,
+    description: `This is my coding blog where I write about my coding journey.`,
+  },
+
+  plugins: [
+
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+      },
+    },
+
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/posts`,
+        name: `posts`,
+      },
+    },
+  ],
+};
