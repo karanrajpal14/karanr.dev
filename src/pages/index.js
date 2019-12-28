ae333dfff82720555ee9a55923e2b3d007ca05cb
Karan Rajpal <rajpal.karan73@gmail.com>
2019-12-28 16:10:42 -0600

    Added hook to query site metadata

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from "react"
+import { useSiteMetadata } from '../hooks/useSiteMetadata';
 
-export default () => <div>Hello world!</div>
+export default () => {
+    const { title, description } = useSiteMetadata();
+    return (
+        <React.Fragment>
+            <h1>{title}</h1>
+            <p>{description}</p>
+        </React.Fragment>
+    );
+}
