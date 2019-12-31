834e3ad55ff538e71a8856566e23ab9b37a90e48
Karan Rajpal <rajpal.karan73@gmail.com>
2019-12-31 15:55:21 -0600

    Added a button to copy code snippets to clipboard

--- src/components/Code.js
+++ src/components/Code.js
@@ -2,19 +2,20 @@ import Highlight, { defaultProps } from 'prism-react-renderer'
 import theme from 'prism-react-renderer/themes/nightOwl'
 import React from 'react'
 import styled from 'styled-components';
+import { copyToClipboard } from '../utils/copy-to-clipboard';
 
 export const Pre = styled.pre`
   text-align: left;
-  margin: 1em 0;
-  padding: 0.5em;
+  margin: 1rem 0;
+  padding: 0.5rem;
   overflow-x: auto;
   border-radius: 3px;
   & .token-line {
-    line-height: 1.3em;
-    height: 1.3em;
+    line-height: 1.3rem;
+    height: 1.3rem;
   }
-
   font-family: 'Courier New', Courier, monospace;
+  position: relative;
 `;
 
 export const LineNo = styled.span`
@@ -24,7 +25,23 @@ export const LineNo = styled.span`
   opacity: 0.3;
 `;
 
+const CopyCode = styled.button`
+  position: absolute;
+  right: 0.25rem;
+  border: 0;
+  border-radius: 3px;
+  margin: 0.25em;
+  opacity: 0.3;
+  &:hover {
+    opacity: 1;
+  }
+`;
+
 export const Code = ({ codeString, language }) => {
+    const handleClick = () => {
+        copyToClipboard(codeString);
+    };
+
     return (
         <Highlight
             {...defaultProps}
@@ -40,6 +57,7 @@ export const Code = ({ codeString, language }) => {
                 getTokenProps,
             }) => (
                     <Pre className={className} style={style}>
+                        <CopyCode onClick={handleClick}>Copy</CopyCode>
                         {tokens.map((line, i) => (
                             <div {...getLineProps({ line, key: i })}>
                                 <LineNo>{i + 1}</LineNo>
