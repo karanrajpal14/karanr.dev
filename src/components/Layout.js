e401ba358cc8eef3a44eda7c34e617a5fdc581c4
Karan Rajpal <rajpal.karan73@gmail.com>
2020-02-05 00:41:37 -0600

    Disabled page transitions

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,17 +3,14 @@ import { Navbar } from "./Navbar"
 import { Footer } from "./Footer"
 import { useSiteMetadata } from "../hooks/useSiteMetadata"
 import "./layout.scss"
-import PageTransition from "gatsby-v2-plugin-page-transitions"
 
 export const Layout = ({ children }) => {
   const { title, description, authorName } = useSiteMetadata()
   return (
-    <PageTransition transitionTime={500}>
       <React.Fragment>
         <Navbar />
         <main class="site-content">{children}</main>
         <Footer authorName={authorName} />
       </React.Fragment>
-    </PageTransition>
   )
 }
