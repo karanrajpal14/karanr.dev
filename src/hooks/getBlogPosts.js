e790f88fef9ec4f750ef439ace1d07f3c964b6b6
Karan Rajpal <rajpal.karan73@gmail.com>
2020-03-20 18:18:19 -0500

    Cleanup

---
+++ src/hooks/getBlogPosts.js
@@ -0,0 +1,37 @@
+import { graphql, useStaticQuery } from "gatsby"
+
+export const getBlogPosts = () => {
+  const data = useStaticQuery(graphql`
+    {
+      allMdx(
+        limit: 3
+        sort: { fields: [frontmatter___date], order: DESC }
+        filter: {
+          frontmatter: { published: { eq: true }, type: { eq: "post" } }
+        }
+      ) {
+        nodes {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            title
+            date(formatString: "YYYY MMMM Do")
+            tags
+            cover {
+              publicURL
+              childImageSharp {
+                sizes(maxWidth: 1500, traceSVG: { color: "#639" }) {
+                  ...GatsbyImageSharpSizes_tracedSVG
+                }
+              }
+            }
+          }
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  `)
+  return data.allMdx.nodes
+}
