8be0876f6a01f465502a78752b413b67804eeda2
Karan Rajpal <rajpal.karan73@gmail.com>
2021-04-08 23:37:38 -0500

    Updated work experience

--- src/components/Work.js
+++ src/components/Work.js
@@ -10,6 +10,37 @@ export const Work = () => {
       : false
   }
 
+  const TimelineHeader = ({ year }) => {
+    return (
+      <header className="timeline-header">
+        <span className="tag is-primary">{year}</span>
+      </header>
+    )
+  }
+
+  const TimelineItem = ({ startDate, organization, role, color }) => {
+    return (
+      <div className="timeline-item">
+        <div className={`timeline-marker is-${color}`}></div>
+        <div className="timeline-content">
+          <Title className="heading" size="6">
+            {startDate}
+          </Title>
+          <Card backgroundColor={color}>
+            <Card.Content>
+              <Title textColor="light" size="5">
+                {organization}
+              </Title>
+              <Title textColor="light" size="6" subtitle>
+                {role}
+              </Title>
+            </Card.Content>
+          </Card>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <Section className="primary-light-bg" id="work">
       <Column size="half" offset="one-quarter">
@@ -18,65 +49,52 @@ export const Work = () => {
         </Title>
         <Container fluid>
           <div className={`timeline ${isMobile() ? "" : "is-centered"}`}>
-            <header className="timeline-header">
-              <span className="tag is-primary">2016</span>
-            </header>
-            <div className="timeline-item is-techm">
-              <div className="timeline-marker is-techm"></div>
-              <div className="timeline-content">
-                <Title className="heading" size="6">
-                  August 2016
-                </Title>
-                <Card backgroundColor="techm">
-                  <Card.Content>
-                    <Title textColor="light">Tech Mahindra</Title>
-                    <Title textColor="light" subtitle>
-                      Associate Software Engineer
-                    </Title>
-                  </Card.Content>
-                </Card>
-              </div>
-            </div>
-            <header className="timeline-header">
-              <span className="tag is-primary">2017</span>
-            </header>
-            <div className="timeline-item is-iisc">
-              <div className="timeline-marker is-iisc"></div>
-              <div className="timeline-content">
-                <Title className="heading" size="6">
-                  June 2017
-                </Title>
-                <Card backgroundColor="iisc">
-                  <Card.Content>
-                    <Title textColor="light">
-                      Indian Institute of Science (IISc)
-                    </Title>
-                    <Title textColor="light" subtitle>
-                      Project Assistant
-                    </Title>
-                  </Card.Content>
-                </Card>
-              </div>
-            </div>
-            <header className="timeline-header">
-              <span className="tag is-primary">2019</span>
-            </header>
-            <div className="timeline-item is-propelld">
-              <div className="timeline-marker is-propelld"></div>
-              <div className="timeline-content">
-                <Title className="heading" size="6">
-                  August 2019
-                </Title>
-                <Card backgroundColor="propelld">
-                  <Card.Content>
-                    <Title textColor="light">Propelld</Title>
-                    <Title textColor="light" subtitle>
-                      Software Developer
-                    </Title>
-                  </Card.Content>
-                </Card>
-              </div>
-            </div>
+            <TimelineHeader year="2016" />
+            <TimelineItem
+              startDate="August 2016"
+              organization="Tech Mahindra"
+              role="Associate Software Engineer"
+              color="techm"
+            />
+            <TimelineHeader year="2017" />
+            <TimelineItem
+              startDate="June 2017"
+              organization="Indian Institute of Science (IISc)"
+              role="Project Assistant (Software)"
+              color="iisc"
+            />
+            <TimelineHeader year="2019" />
+            <TimelineItem
+              startDate="August 2019"
+              organization="Propelld"
+              role="Software Developer"
+              color="propelld"
+            />
+            <TimelineHeader year="2020" />
+            <TimelineItem
+              startDate="April 2020"
+              organization="TRIO Upward Bound Program - UT Arlington"
+              role="STEM Tutor"
+              color="trio"
+            />
+            <TimelineItem
+              startDate="May 2020"
+              organization="ThoughtFocus Inc."
+              role="Software Developer Intern"
+              color="tf"
+            />
+            <TimelineItem
+              startDate="August 2020"
+              organization="Google Developer Student Clubs - UT Arlington"
+              role="Technical Lead"
+              color="dsc"
+            />
+            <TimelineItem
+              startDate="September 2020"
+              organization="College of Business Administration - UT Arlington"
+              role="Graduate Teaching Assistant"
+              color="gta"
+            />
           </div>
         </Container>
       </Column>
